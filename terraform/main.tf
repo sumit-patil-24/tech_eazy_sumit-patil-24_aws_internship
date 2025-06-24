2133c709348df8eea4a8ad052d8c6bb67eba159f
Your Name <you@example.com>
2025-06-24 22:02:01 +0530

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,6 +39,13 @@ resource "aws_security_group" "mysg" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "ssh"
     from_port = 22
     to_port = 22
